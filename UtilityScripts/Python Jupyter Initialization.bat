02c8dcf7cc0297d731ea707bf9d7f3ddb2ae2426
Austin Shearin <austinmshearin@gmail.com>
2024-02-19 14:03:36 -0600

    Updated output for clarity

--- UtilityScripts/Python Jupyter Initialization.bat
+++ UtilityScripts/Python Jupyter Initialization.bat
@@ -6,7 +6,7 @@ set VirEnvName=%CurrDirName%_VirEnv
 call "./%VirEnvName%/Scripts/activate.bat"
 echo Adding Python virtual environment to Jupyter notebooks: %VirEnvName%
 set /p dummy=Press enter to add Python virtual environment or close to cancel
-echo Adding Python virtual environment
+echo Adding Jupyter to Python virtual environment
 python -m ipykernel install --user --name="%VirEnvName%"
 echo Python virtual environment added
 set /p dummy=Press enter to close
