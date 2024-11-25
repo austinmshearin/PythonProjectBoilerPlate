c2bbb3be31889b36887ecab06a195426e4eadba2
Austin Shearin <ashearin@expr.net>
2024-11-25 08:08:28 -0600

    Overall package maintenance: updated docker compose files, added utility scripts to visual studio code tasks, started universal spell check dictionary, etc.

--- setup.py
+++ setup.py
@@ -1,15 +1,12 @@
 import setuptools
 
-with open('README.md', 'r', encoding='utf-8') as read_me_file:
-    long_description = read_me_file.read()
-
 setuptools.setup(
     name='package',
     version='0.0.1',
     author='Author',
     author_email='author@gmail.com',
-    description='Python Project Boiler Plate',
-    long_description=long_description,
+    description='Python Project Boiler Plate Package',
+    long_description='Python Project Boiler Plate Package',
     long_description_content_type='text/markdown',
     url='https://github.com/author/project.git',
     license='MIT',
